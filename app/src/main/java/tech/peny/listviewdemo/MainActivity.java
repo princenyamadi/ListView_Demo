a92eb1d424747ba6b8d9472d9f274e7e33e1d2bf
princenyamadi <princenyamadi.pn@gmail.com>
2020-11-29 21:39:48 +0000

    [feat]: Added layout and code
    Added a ListView container, created an aray to populate the listview

--- app/src/main/java/tech/peny/listviewdemo/MainActivity.java
+++ app/src/main/java/tech/peny/listviewdemo/MainActivity.java
@@ -3,12 +3,40 @@ package tech.peny.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+        ArrayList<String> myFamily = new ArrayList<String>();
+
+        myFamily.add("Prince");
+        myFamily.add("Hilda");
+        myFamily.add("John");
+        myFamily.add("Jack");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,myFamily);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Person Selected", myFamily.get(position));
+            }
+        });
     }
 }
